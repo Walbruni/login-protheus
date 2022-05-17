bed6cb94d2cb271e329cb3ff73757a138f4bac03
walbrunilucas1 <lucaswalbruni@gmail.com>
2022-05-16 22:24:35 -0300

    finaliza tela login com api

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 16px 20px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 16px;
+  max-width: 500px;
+  background-color: #b6bdbf;
+  border: 1px solid #b6bdbf;
+  box-shadow: 0 4px 8px 0 rgb(0 0 0 / 15%);
+  color: #fff;
+`;
---
+++ src/components/Input/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  outline: none;
+  padding: 0 16px;
+  width: 100%;
+  height: 44px;
+  font-size: 16px;
+  line-height: 24px;
+  background-color: #fff;
+  border: 1px solid #b6bdbf;
+  border-radius: 3px;
+  box-shadow: inset 0 1px 8px 0 rgb(0 0 0 / 10%);
+`;
